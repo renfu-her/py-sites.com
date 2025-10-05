daa74083431270dfe168c939cf663fac7d8d3dbe
Patrick Her <renfu.her@gmail.com>
2025-10-05 19:59:49 +0800

    Update database password retrieval in Flask app to use a default value of 'py-password' for improved security and consistency.

--- apps/main/app.py
+++ apps/main/app.py
@@ -8,7 +8,7 @@ from sqlalchemy.exc import SQLAlchemyError
 app = Flask(__name__)
 
 db_user = os.getenv("DB_USER", "py-app")
-db_pass = quote_plus(os.getenv("DB_PASSWORD", ""))  # 密碼安全編碼
+db_pass = os.getenv("DB_PASSWORD", "py-password")
 db_host = os.getenv("DB_HOST", "mariadb")
 db_port = os.getenv("DB_PORT", "3306")
 db_name = os.getenv("DB_NAME", "main_db")
